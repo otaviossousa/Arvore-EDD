8a7a7dca7abb1a2958dd621c458a8be12b1f4baf
Otavio Sousa <otaviosousamartins14@gmail.com>
2024-03-24 20:45:31 -0300

    add: functions buscarNo remover alturaArv alturaSubArv

--- Arvore_Binaria_Ponteiro.c
+++ Arvore_Binaria_Ponteiro.c
@@ -168,6 +168,62 @@ int buscar(No *raiz, int chave){
      }
 }
 
+No* buscarNo(No *raiz, int chave){
+    if(raiz == NULL)
+        return NULL;
+    else{
+        if(raiz->conteudo == chave)
+            return raiz;
+        else{
+            if(chave < raiz->conteudo)
+                return buscarNo(raiz->esquerda,chave);
+            else
+                return buscarNo(raiz->direita,chave);
+        }
+    }
+}
+
+No* remover(No *raiz, int chave){
+    if(raiz ==NULL){
+        printf("Valor nao encontrado!\n");
+        return NULL;
+    }else{
+        if(raiz->conteudo == chave){
+            // Remove nós folhas
+            if(raiz->esquerda == NULL && raiz->direita == NULL){
+                free(raiz);
+                return NULL;
+            }else{
+                // Remove nós que possuem apenas 1 filho
+                if(raiz->esquerda == NULL || raiz->direita ==NULL){
+                    No *aux;
+                    if(raiz->esquerda != NULL)
+                        aux = raiz->esquerda;
+                    else
+                        aux = raiz->direita;
+                    free(raiz);
+                    return aux;
+                }else{
+                    No* aux = raiz->esquerda;
+                    while (aux->direita != NULL)
+                        aux = aux->direita;
+                    raiz->conteudo = aux->conteudo;
+                    aux->conteudo = chave;
+                    raiz->esquerda = remover(raiz->esquerda,chave);
+                    return raiz;
+                }
+            }
+        } else{
+            if(chave < raiz->conteudo)
+                raiz->esquerda = remover(raiz->esquerda,chave);
+            else
+                raiz->direita = remover(raiz->direita,chave);
+            return raiz;
+        }
+    }
+    return NULL;
+}
+
 //Funçao para imprimir os elementos da arvore
 void imprimir(No *raiz){
     if(raiz != NULL){
@@ -177,13 +233,36 @@ void imprimir(No *raiz){
     }
 }
 
+// Funcao para calcular a altura de uma árvore binária
+int alturaArv(No *raiz){
+    // se raiz for nula ou folha
+    if(raiz == NULL || raiz->direita == NULL && raiz->esquerda == NULL)
+        return 0;
+    else{
+        int esq = 1 + alturaArv(raiz->esquerda);
+        int dir = 1 + alturaArv(raiz->direita);
+        //retorna o maior
+        if(esq > dir)
+            return esq;
+        else
+            return dir;
+    }
+}
+
+int alturaSubArv(No *raiz,int chave){
+    No *no = buscarNo(raiz,chave);
+    if(no)
+        return alturaArv(no);
+    else
+        return -1;
+}
 
 int main(){
     int op,valor;
     No *raiz = NULL;
 
     do {
-        printf("\n0 - Sair\n1 - inserir\n2 - Imprimir\n3- Buscar\n");
+        printf("\n0 - Sair\n1 - inserir\n2 - Imprimir\n3 - Buscar\n4 - Remover\n5 - Altura\n6 - Altura subarvore\n");
         scanf("%d",&op);
         switch (op) {
             case 0:
@@ -200,10 +279,18 @@ int main(){
                 printf("\n");
                 printf("Tamanho: %d\n", tamanho(raiz));
                 break;
-            case 3:
-                printf("Qual valor deseja buscar?");
+            case 4:
+                printf("Digite um valor a ser removido:");
                 scanf("%d",&valor);
-                printf("Resultado da busca: %d\n", buscar(raiz,valor));
+                raiz = remover(raiz,valor);
+                break;
+            case 5:
+                printf("A altura da arvore e: %d\n", alturaArv(raiz));
+                break;
+            case 6:
+                printf("Qual o valor deseja calcular a altura? ");
+                scanf("%d",&valor);
+                printf("\nA altura e: %d\n", alturaSubArv(raiz,valor));
                 break;
             default:
                 printf("\n Opcao invalida!\n");
